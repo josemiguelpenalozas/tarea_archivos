d58bea92e87c6ea345628799659495f0b49b09da
Jose_Miguel <jo.penalozas@duocuc.cl>
2024-05-31 21:26:12 -0400

    Aqui se avanzo para pasar la informacion del archivo de texto a un archivo csv

--- tarea_arch.py
+++ tarea_arch.py
@@ -2,8 +2,17 @@ import csv
 import json
 import os
 
+x=0
 os.system("cls")
 archivo_a_leer="listado.txt"
 with open(archivo_a_leer,"r") as archivo:
     contenido=archivo.read()
-    print(contenido)
+    contenido=contenido.split("\n")
+
+    while x<len(contenido):
+        escritura=contenido[x].split(",")
+        with open("listado.csv","a",newline="") as excel:
+            escritor_csv = csv.writer(excel)
+            escritor_csv.writerow(escritura)
+
+        x=x+1

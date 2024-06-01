151c818ea6cbe3677c7e740cc9af7bf0f598b3bd
Jose_Miguel <jo.penalozas@duocuc.cl>
2024-05-31 22:42:24 -0400

    Aqui se saco la informacion del archivo csv a un archivo json agregando una llave extra

--- tarea_arch.py
+++ tarea_arch.py
@@ -16,3 +16,27 @@ with open(archivo_a_leer,"r") as archivo:
             escritor_csv.writerow(escritura)
 
         x=x+1
+
+with open("listado.csv","r",newline="") as excel:
+    lector_cvs=csv.reader(excel)
+    for x in lector_cvs:
+        num=int(x[2])
+        if num>200000000:
+            empresa={"rut":x[0],
+                     "nombre":x[1],
+                     "ventas":x[2],
+                     "contribuyente":"Gran Contribuyente"}
+        elif num<=200000000 and num>100000000 :
+            empresa={"rut":x[0],
+                     "nombre":x[1],
+                     "ventas":x[2],
+                     "contribuyente":"Mediano Contribuyente"}
+        else:
+            empresa={"rut":x[0],
+                     "nombre":x[1],
+                     "ventas":x[2],
+                     "contribuyente":"Pequeno Contribuyente"}
+        with open("Listado.json","+a") as diccionario:
+            json.dump(empresa,diccionario)
+            diccionario.write("\n")
+            
